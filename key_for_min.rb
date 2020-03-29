cd49e306b343051954bdab1233f4030764155a33
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-29 09:37:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,17 @@
 def key_for_min_value(name_hash)
 
-  min_name = nil
-  min_value = nil
+  min_name == nil
+  min_value == nil
 
   name_hash.each do |name,value|
     min_name = name if min_name == nil
     min_value = value if min_value == nil
     if value < min_value
-      min_value = value
       min_name = name
+      min_value = value
     end
   end
 
   min_name
-
+  
 end
